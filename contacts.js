e79ef3a2230835eb5d07b15626a79445ed991f74
Valentine Pylypchuk <strheadshot1997@gmail.com>
2023-07-07 17:01:25 +0300

    update functions

--- contacts.js
+++ contacts.js
@@ -1,29 +1,51 @@
 const fs = require("fs/promises");
 const path = require("path");
+const {nanoid} = require("nanoid");
 
 const contactsPath = path.join(__dirname, "db", "contacts.json");
 
-/** Get all contacts from contacts.json (array) */
+/** Get all contacts from contacts.json (array).
+ * @returns contact list.
+ */
 const listContacts = async () => {
   const data = await fs.readFile(contactsPath);
   return JSON.parse(data);
 };
 
-/** Get one contact by id */
+/** Get one contact by id.
+ * @returns found contact.
+ */
 const getContactById = async (contactId) => {
   const contacts = await listContacts();
   const foundСontact = contacts.find((contact) => contact.id === contactId);
   return foundСontact || null;
 };
 
-/** Remove one contact by id */
+/** Remove one contact by id.
+ * @returns removed contacts.
+*/
 const removeContact = async (contactId) => {
-  const data = await fs.readFile(contactsPath);
+  const contacts = await listContacts();
+  const foundСontact = contacts.findIndex((contact) => contact.id === contactId);
+  if (foundСontact === -1) return null;
+  const removedContact = contacts.splice(foundСontact, 1);
+  return removedContact || null;
 };
 
-/** Add a new contact into contacts.json */
+/** Add a new contact into contacts.json.
+ * @returns new contact.
+ */
 const addContact = async (name, email, phone) => {
-  const data = await fs.readFile(contactsPath);
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone
+  }
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
 };
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const contacts = require("./contacts");
 
-// const get = async () => {
-// const getAll = await contacts.listContacts();
-// return console.log(getAll);
-// }
+const getContactList = async () => {
+const getAllContacts = await contacts.listContacts();
+return console.log(getAllContacts);
+}
 
-// get();
+// getContactList();
 
-const getContact = async (id) => {
-  const foundContact = await contacts.getContactById(id);
-  return console.log(foundContact);
+// const getContact = async (id) => {
+//   const foundContact = await contacts.getContactById(id);
+//   return console.log(foundContact);
+// };
+
+// getContact("AeHIrLTr6JkxGE6SN-0Rw");
+
+
+// const removeContactById = async (id) => {
+//   const removedContact = await contacts.removeContact(id);
+//   return console.log(removedContact);
+// };
+
+// removeContactById("j__Wzuze1dLVBODXEoR4K");
+
+const addContact = async (...data) => {
+const newContacts = await contacts.addContact(...data);
+return console.log(getContactList());
 };
 
-getContact("AeHIrLTr6JkxGE6SN-0Rw");
+addContact("Mango", "mamama@mail.com", "722-22-880");
